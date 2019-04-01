c31a5e848aa11727c8c5881df6a772215ac75e5d
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2019-04-01 10:23:47 +0600

    Throws statement corrected

--- Assignment 2/src/exceptionHandling/Throws.java
+++ Assignment 2/src/exceptionHandling/Throws.java
@@ -1,26 +1,11 @@
 package exceptionHandling;
 
-public class Throws {
-	
-	static int x =20, y =0;
-	
-	static void test() throws ArithmeticException 
-    { 
-        System.out.println("Inside method " + x/y); 
-        throw new ArithmeticException("divided by zero"); 
-    } 
-	
-	public static void main(String[] args) {
+public class Throws {	
 		
-		try
-        { 
-            test(); 
-        } 
-        catch(ArithmeticException e) 
-        { 
-            System.out.println("caught in main."); 
-        } 
-		
-	}
+	public static void main(String[] args)throws InterruptedException 
+	{ 
+		Thread.sleep(10000); 
+		System.out.println("SUST"); 
+	} 
 
 }
